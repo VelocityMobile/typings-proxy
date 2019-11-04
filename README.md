e6cd9c0009948eb01e03869feb73428897b90708
Sune Westphalen <sune.westphalen@velocityapp.com>
2019-11-04 11:28:47 -0800

    feat: simple proxy

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # typings-proxy
 Because typings.org is expired, we will proxy our own mirror to their IP.
+
+After deploying this, update/create `.typingsrc` in your project and point it to this proxy, e.g.
+```
+registryURL=typings.velocityapp.com
+```
---
+++ index.js
@@ -0,0 +1,20 @@
+// Based on https://gist.github.com/e0ne/3156463
+var http = require('http')
+
+http.createServer(function(request, response){
+    var request_options = {
+        host: '104.24.112.177', // Thank you https://securitytrails.com/domain/typings.org/history
+        port: 80,
+        path: request.url,
+        method: request.method,
+        headers: {
+            host: 'api.typings.org', // Spoof the host
+        }
+    }
+    var proxy_request = http.request(request_options, function(proxy_response){
+        proxy_response.pipe(response)
+        //proxy_response.on('data', function (chunk) => {})
+        response.writeHead(proxy_response.statusCode, proxy_response.headers)
+    })
+    request.pipe(proxy_request)
+}).listen(80)
